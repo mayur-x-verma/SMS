24c27a631349d220691a37e13e6ec9eb79297509
Admin\Administrator <vermaxmayur100@gmail.com>
2025-04-29 13:11:25 +0530

    Basic setup

---
+++ Student-management-system/views/student/login.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = 'Student Login';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        This is the Student login page. You may modify the following file to customize its content:
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
